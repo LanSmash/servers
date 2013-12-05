30aa6192a98955c10a4a38fda874bef743e0f704
webberist <me@webberist.com>
2013-12-05 12:33:13 +1000

    finished blacklist script and updated bind initalise to include blacklist generation

--- bind/initialise.sh
+++ bind/initialise.sh
@@ -1,4 +1,13 @@
 apt-get install bind9 dnsutils
 cd /etc/bind
 ln -s -f /home/weby/servers/bind/* .
+touch /etc/bind/named.conf.blocklist
 /etc/init.d/bind9 restart
+#cd ../blacklist
+mkdir -p /tmp/blacklist
+cd /tmp/blacklist
+~/servers/blacklist/blacklist.sh
+cp named.conf.blocklist /etc/bind
+
+#reloading bind, this takes a few minutes
+time rndc reload
