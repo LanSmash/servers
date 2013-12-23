49d09c9e7799ef47cef038adce845d4a5633ca39
Webberist <me@webberist.com>
2013-12-22 16:42:19 -0800

    fixed username in git message

--- initialise.sh
+++ initialise.sh
@@ -3,13 +3,15 @@ git config --global user.name "Webberist"
 git config --global user.email me@webberist.com
 
 #use github with ssh
-ssh-keygen -t rsa -C "server@webberist.com"
+[ ! -f ~/.ssh/id_rsa ] && ssh-keygen -t rsa -C "server@webberist.com"
+echo "#############################################"
 cat ~/.ssh/id_rsa.pub
+echo "#############################################"
 echo copy paste this key into github:
 echo https://github.com/settings/ssh
 echo press enter to continue
-read
-git clone webberist@github.com:webberist/servers.git ~/servers
+read novariable
+git clone git@github.com:webberist/servers.git ~/servers
 
 #use github with https
 #git clone https://github.com/webberist/servers.git servers
