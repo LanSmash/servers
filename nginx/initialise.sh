cb4e6da564ceb85a484c93a3eac977da49cdde16
Webberist <me@webberist.com>
2013-12-28 17:57:19 +1000

    Added openssl certificate generation for nginx https setup

--- nginx/initialise.sh
+++ nginx/initialise.sh
@@ -3,4 +3,12 @@ ln -s -f /home/weby/servers/nginx/nginx.conf /etc/nginx/nginx.conf
 mkdir -p /var/proxy/nginx/steampipe/depot
 mkdir -p /var/proxy/nginx/wargaming
 chown -R www-data /var/proxy/nginx
-ln -s -f /home/weby/servers/nginx/www/* /var/www
+ln -s -f /home/weby/servers/nginx/www /var/www
+
+echo generating SSL certificate
+cd /etc/nginx/
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr -days 3650 -subj '/C=AU/ST=AU/L=Event/CN=Proxy.lansmash'
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+/etc/init.d/nginx restart
+
