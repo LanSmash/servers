504429340a50bc66bb4c7a6f729b733ff571e510
Webberist <me@webberist.com>
2013-12-29 10:39:45 +1000

    adding log file rules

--- bind/initialise.sh
+++ bind/initialise.sh
@@ -1,14 +1,16 @@
 apt-get install bind9 dnsutils
 cd /etc/bind
 ln -s -f /home/weby/servers/bind/* .
-touch /etc/bind/named.conf.blocklist
+
+touch /var/log/bind.log
+chown bind /var/log/bind.log
+
 /etc/init.d/bind9 restart
 #cd ../blacklist
+echo Bind installed successfully.
+echo Blacklist generation will now commence, abort if there is no blacklist required
+echo press enter to continue
+read novariable
 mkdir -p /tmp/blacklist
 cd /tmp/blacklist
 ~/servers/blacklist/blacklist.sh
-rm /etc/bind/named.conf.blocklist
-cp named.conf.blocklist /etc/bind
-
-echo reloading bind, this takes a few minutes
-rndc reload
