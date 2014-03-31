015cf2db9ccd3249becdd56788fe0f292f509f9e
Webberist <me@webberist.com>
2014-03-31 20:56:10 +1000

    updated squid3 to work better with DNS RPZ, nginx partial caching proxy, and cleaned comments.  uncomment peer cache to reuse squid for url forwarding

--- squid3/initialise.sh
+++ squid3/initialise.sh
@@ -2,8 +2,9 @@ ln -s -f /home/weby/servers/squid3/squid.conf /etc/squid3/squid.conf
 mkdir -p /var/proxy/squid
 chown -R proxy:proxy /var/proxy/squid
 
-#add steamproxypeer to point to localhost
-cp /etc/hosts /etc/hosts.backup
-grep -v "steamproxypeer" /etc/hosts.backup > /etc/hosts
-echo "127.0.0.1 steamproxypeer" >>/etc/hosts
+## Use DNS for steamcache
+#add steamcache to point to localhost
+#cp /etc/hosts /etc/hosts.backup
+#grep -v "steamcache" /etc/hosts.backup > /etc/hosts
+#echo "127.0.0.1 steamcache" >>/etc/hosts
 
