585a6fbc7dd2baceac72daea20718905b6797bcc
Webberist <me@webberist.com>
2014-03-31 22:54:01 +1000

    added new nginx-www config for proxy.ls nginx with just wpad and block.ls

---
+++ nginx-www/initialise.sh
@@ -0,0 +1,15 @@
+apt-get install nginx
+ln -s -f /home/weby/servers/nginx/nginx.conf /etc/nginx/nginx.conf
+mkdir -p /var/proxy/nginx/steampipe/depot
+mkdir -p /var/proxy/nginx/wargaming
+chown -R www-data /var/proxy/nginx
+mv /var/www /var/www-disabled
+ln -s -f /home/weby/servers/nginx/www /var/www
+
+echo generating SSL certificate
+cd /etc/nginx/
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr -days 3650 -subj '/C=AU/ST=AU/L=Event/CN=Proxy.lansmash'
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+/etc/init.d/nginx restart
+
