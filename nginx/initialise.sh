1512122af822f5999894e04c5b9cfd210dcf2281
Webberist <me@webberist.com>
2014-03-31 23:03:17 +1000

    tidied nginx for cache.ls

--- nginx/initialise.sh
+++ nginx/initialise.sh
@@ -1,12 +1,7 @@
 apt-get install nginx
-ln -s -f /home/weby/servers/nginx-www/nginx.conf /etc/nginx/nginx.conf
-mv /var/www /var/www-disabled
-ln -s -f /home/weby/servers/nginx-www/www /var/www
+ln -s -f /home/weby/servers/nginx/nginx.conf /etc/nginx/nginx.conf
+chown -R www-data /var/proxy
 
-echo generating SSL certificate
-cd /etc/nginx/
-openssl genrsa -out server.key 2048
-openssl req -new -key server.key -out server.csr -days 3650 -subj '/C=AU/ST=AU/L=Event/CN=Proxy.lansmash'
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
 /etc/init.d/nginx restart
 
