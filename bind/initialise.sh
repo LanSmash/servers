fcd149593a201ead78658c1b996fad49c0e9b62d
Webberist <me@webberist.com>
2014-07-03 21:07:56 +1000

    readded dnssec, decreased log size

--- bind/initialise.sh
+++ bind/initialise.sh
@@ -5,6 +5,11 @@ ln -s -f /home/weby/servers/bind/* .
 touch /var/log/bind.log
 chown bind /var/log/bind.log
 
+# to allow bind to rotate logs, probably a better way of doing this, like a different directory
+cd /var/log
+sudo chgrp bind .
+sudo chmod g+w .
+
 /etc/init.d/bind9 restart
 #cd ../blacklist
 echo Bind installed successfully.
