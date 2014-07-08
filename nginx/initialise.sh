8fb8f8572e857c93d9814220e97683f9752eb7d2
root <root@techdrive.lan>
2014-07-08 19:26:36 +1000

    added some mkdir stuff to init script

--- nginx/initialise.sh
+++ nginx/initialise.sh
@@ -1,6 +1,15 @@
+
+
 apt-get install nginx
+
+#webby only
 ln -s -f /home/weby/servers/nginx/nginx.conf /etc/nginx/nginx.conf
-chown -R www-data /var/proxy
+
+#better hope /var/proxy has some diskspace 
+mkdir -p /var/proxy/nginx/cs.steampowered.com-proxy
+mkdir -p /var/proxy/nginx/l3cdn.riotgames.com
+mkdir -p /var/proxy/nginx/blizzard.com
+chown -R www-data:www-data /var/proxy
 
 
 /etc/init.d/nginx restart
