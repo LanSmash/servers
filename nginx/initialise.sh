9cd0fec98a4a2db14ae507795b5af5ac184936eb
Webberist <me@webberist.com>
2015-04-06 21:48:46 +1000

    updated nginx initialise.sh

--- nginx/initialise.sh
+++ nginx/initialise.sh
@@ -1,3 +1,7 @@
+sudo sh -c 'echo deb http://ppa.launchpad.net/nginx/stable/ubuntu lucid main > /etc/apt/sources.list.d/nginx-stable-lucid.list'
+#todo add lines to update and get keys for new ppa above
+#todo - apt install nginx
+
 mkdir -p /data/www/logs
 chown -R nginx:nginx /data/www
 echo 10.0.0.53	lancache-steam >> /etc/hosts
@@ -8,4 +12,5 @@ echo 10.0.0.57  lancache-origin >> /etc/hosts
 echo 10.0.0.58  lancache-sony >> /etc/hosts
 echo 10.0.0.59  lancache-origin >> /etc/hosts
 cd /etc/nginx && ln -s -f /home/weby/servers/nginx/* .
+useradd nginx
 
